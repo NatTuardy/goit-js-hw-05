5e393cc499bda373b95096abef49cfe68d38b78a
Your Name <you@example.com>
2020-03-18 14:38:00 +0200

    added homework-5

---
+++ js/task-02.js
@@ -0,0 +1,32 @@
+"use strict";
+// Напиши класс User для создания пользователя со следующим свойствами:
+class User {
+  constructor({ name, age, followers }) {
+    this.name = name;
+    this.ageNumber = age;
+    this.followersNumber = followers;
+  }
+  getInfo() {
+    console.log(
+      `User ${this.name} is ${this.ageNumber} years old and has ${this.followersNumber} followers`
+    );
+  }
+}
+// Добавь метод getInfo(), который, выводит строку: User ${имя}
+//  is ${возраст} years old and has ${кол-во фоловеров} followers
+
+const mango = new User({
+  name: "Mango",
+  age: 2,
+  followers: 20
+});
+
+mango.getInfo(); // User Mango is 2 years old and has 20 followers
+
+const poly = new User({
+  name: "Poly",
+  age: 3,
+  followers: 17
+});
+
+poly.getInfo(); // User Poly is 3 years old and has 17 followers
